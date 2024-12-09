b153fddb1b415e9f4dc18b512ef527b6cd0f392d
Jesus Fernandez <Jesus.Fernandez@unican.es>
2024-12-09 20:45:16 +0100

    Use providers version if available
    only make up version from file modification date if no version folder was provided.

--- drs_hierarchy.sh
+++ drs_hierarchy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#BASE_DIR="/work/bg1369/FPS-URB-RCC/PARIS-12"
-#BASE_DIR="/work/bg1369/FPS-URB-RCC/EUR-12"
-BASE_DIR="/work/bg1369/FPS-URB-RCC/PARIS-3"
+domain=${1:-PARIS-3}
+
+BASE_DIR="/work/bg1369/FPS-URB-RCC/${domain}"
 OUTPUT_DIR="/home/b/b382580/FPS-URB-RCC/b382580_jfernandez"
 
 for file in $(find ${BASE_DIR} -type f -name '*.nc' | sort); do
@@ -27,10 +27,16 @@ for file in $(find ${BASE_DIR} -type f -name '*.nc' | sort); do
     filename=${filename//.nc/}
     IFS="_" read -r variable_id domain_id driving_source_id driving_experiment_id driving_variant_label institution_id source_id version_realization frequency dates <<< "$filename"
 
-    # Get the file modification time and format it as vYYYYMMDD
-    mod_time=$(stat -c %y "${fullpath}" | cut -d' ' -f1 | tr -d '-')
-    formatted_version="v${mod_time}"
-
+    # Try to get the version from the path
+    version=$(basename $(dirname "${fullpath}"))
+    if test "${version:0:4}" = "v202"; then
+        formatted_version=${version}
+    else
+        # Otherwise, get the file modification time and format it as vYYYYMMDD
+        mod_time=$(stat -c %y "${fullpath}" | cut -d' ' -f1 | tr -d '-')
+        formatted_version="v${mod_time}"
+    fi
+    
     target_dir="$OUTPUT_DIR/CORDEX/CMIP6/FPS-URB-RCC/${domain_id}/${institution_id}/${driving_source_id}/${driving_experiment_id}/${driving_variant_label}/${source_id}/${version_realization}/${frequency}/${variable_id}/${formatted_version}"
 
     mkdir -p "$target_dir"
